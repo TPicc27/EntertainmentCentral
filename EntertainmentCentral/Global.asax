518f2b6bca836e565acc33427a2f0e2fbd5fa3a4
Tony Piccirilli <anpicc01@louisville.edu>
2018-01-19 10:58:23 -0500

    Add project files.

---
+++ EntertainmentCentral/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntertainmentCentral.MvcApplication" Language="C#" %>
---
+++ EntertainmentCentral/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EntertainmentCentral.Startup))]
+namespace EntertainmentCentral
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
